79a76b0c6df840731b2df8626071e564686eb3ee
Furkan Demir <furkan.vty0@gmail.com>
2023-02-19 18:02:33 +0300

    .

--- Assets/Scenes/CreateWorldPrefab.cs
+++ Assets/Scenes/CreateWorldPrefab.cs
@@ -9,21 +9,20 @@ public class CreateWorldPrefab : MonoBehaviour
     public GameObject cube;
     void Start()
     {
-        for (int x = 0; x < width; x++)        
+        for (int x = 0; x < width; x++)
             for (int z = 0; z < depth; z++)//x*z ebatlarında küplerden oluşan bir görüntü elde etmemizi sağladı.
             {
                 Vector3 pos = new Vector3(x,
-                    Mathf.PerlinNoise(x*0.2f,z*0.2f)*3f//perlin noise ile terrain oluşturmamızı sağlayın değerler arasında yumuşatma oluşturarak 
-                    //random yükseklik değerleri almamızı sağlayan bir algoritmadır.
+                    Mathf.PerlinNoise(x * 0.2f, z * 0.2f) * 3f//perlin noise ile terrain oluşturmamızı sağlayın değerler arasında yumuşatma oluşturarak 
+                                                              //random yükseklik değerleri almamızı sağlayan bir algoritmadır.
                     , z);//x ve z ye göre pozisyonlar almamızı sağlar.
                 GameObject go = Instantiate(cube, pos, Quaternion.identity);
             }
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
         
+
     }
 }
