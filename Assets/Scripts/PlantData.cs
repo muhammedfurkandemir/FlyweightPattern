746d736d0c1163d5d1fbbb9eb34a9acdba2beca2
Furkan Demir <furkan.vty0@gmail.com>
2023-02-27 17:42:40 +0300

    .

--- Assets/Scripts/PlantData.cs
+++ Assets/Scripts/PlantData.cs
@@ -2,7 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName ="plantdata",menuName ="Plant Data",order =51)]
+//bu yapı ile create asset  kısmında bir alan oluşturarak yeni asset oluşturma imaknı verir.file name dosya adını menu name menude gözükecek
+//adı order ise oluşturulacağı kısmı verir.unity en son 60 ye kadar alanı kullandığı için yeni bir alan için 51 i kullandık.
 public class PlantData :ScriptableObject
 {
-    
+    public enum THREAT { none,low,moderate,high}
+
+    [SerializeField]
+    private string plantName;
+    [SerializeField]
+    private THREAT plantThreat;
+    [SerializeField] Texture icon;
 }
